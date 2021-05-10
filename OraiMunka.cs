773bfb0a991298e70e2d5e0c1276b487f858d976
Robert Radnoti <jeromos55@gmail.com>
2021-05-10 14:56:31 +0000

    update 2021-05-10

--- OraiMunka.cs
+++ OraiMunka.cs
@@ -6,76 +6,15 @@ using System.Threading.Tasks;
 
 namespace dotnetcore
 {
-    class Tanulo
+    class FajlAdatok
     {
-        #region Fieald-ek
+        #region Field-ek
         string nev;
-        double atlag;
-        string szemszam;
+        DateTime modositasDatuma;
+        long meretBajtban;
         #endregion
+        // Bemeneti feltételek egyike sem lesz, mivel nem 
 
-        #region Property-k
-        // Ne legyen később módosítható , de ne lehessen üresen létrehozni
-        public string Nev
-        {
-            get => nev;
-            private set
-            { 
-                if (string.IsNullOrWhiteSpace(value))
-                {
-                    nev = "HIBÁS";
-                    // TODO hibakezelés majd később jön ide, ilyenkor hibát kell dobni.
-                }
-                else
-                {
-                    nev = value;    
-                }
-            }
-        }
-        // Később is módosítható legyen, de csak 1 és 5 közötti értéket vehet fel
-        public double Atlag
-        {
-            get => atlag;
-            set
-            {
-                if (value >= 1 && value <= 5)
-                {
-                    atlag = value;
-                }
-                else
-                {
-                    atlag = -1;
-                    // TODO hibakezelés majd később jön ide, ilyenkor hibát kell dobni.
-                }
-            }
-        }
-        // Ne legyen módosítható, de jelenleg nem alkalmazunk rá semmilyen bemeneti feltételt
-        public string Szemszam { get => szemszam; /* set => szemszam = value; */ }  // Ki lehet venni mert nem használjuk vagy 'private set => szemszam = value;'
-
-        public Tanulo(string szemszam, string nev, double atlag)
-        {
-            this.szemszam = szemszam;
-            Nev = nev;
-            Atlag = atlag;
-        }
-        #endregion
-
-        #region Konstruktor
-        public Tanulo(string nev, double atlag, string szemszam)
-        {
-            // Biztosítani kell, hogy a kapott parametereket át tudjuk vezetni az objektum szintű változókba (field-ekbe)
-            Nev = nev; // Nev-nek adjuk nev-et, hogy ne legyen nev = nev ütközés
-            Atlag = atlag;
-            this.szemszam = szemszam; // Szemszam nem fog műköni mert nincs set-je a property-ben ezért kell a this jelölő a mögötte álló dolog az objetum része nem az alprogramé
-        }
-
-        #endregion
-
-        #region Alprogramok
-
-        // Jelenleg ide nem kell semmi
-
-        #endregion
     }
 
 }
